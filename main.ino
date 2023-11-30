08b97d4c9740f0b3469d6b1cf801fd75774e0ebb
empflow <67281212+empflow@users.noreply.github.com>
2023-11-30 15:02:25 +0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,49 +5,44 @@
 
 #define SS_PIN 10
 #define RST_PIN 9
-String UID = "";
+
 const char* AUTHORIZED_UIDS[] = {
   "b3ea810e"
 };
-byte lock = 0;
 
 Servo servo;
 MFRC522 rfid(SS_PIN, RST_PIN);
 
 void setup() {
   Serial.begin(9600);
-  servo.write(70);
   servo.attach(3);
   SPI.begin();
   rfid.PCD_Init();
   rfid.PCD_DumpVersionToSerial();
-  for (int i = 0; i < sizeof(AUTHORIZED_UIDS) / sizeof(AUTHORIZED_UIDS[0]); i++) {
-    Serial.println("Authorized UIDs:");
-    Serial.print(AUTHORIZED_UIDS[i]);
-    Serial.print("");
-  }
-  Serial.println("");
+  servo.write(0);
+  printAuthorizedIds();
 }
 
 void loop() {
-  
   if (!rfid.PICC_IsNewCardPresent()) return;
   if (!rfid.PICC_ReadCardSerial()) return;
 
   String uid = getUid(rfid);
-
   Serial.println(uid);
-  isAuthorized(rfid);
+
+  if (isAuthorized(uid)) {
+    servo.write(150);
+    delay(3000);
+  }
+  servo.write(0);
   
   delay(1500);
 }
 
-bool isAuthorized(MFRC522 rfid) {
-  String providedUid = getUid(rfid);
-
+bool isAuthorized(String uid) {
   int uidsAmount = sizeof(AUTHORIZED_UIDS) / sizeof(AUTHORIZED_UIDS[0]);
   for (int i = 0; i < uidsAmount; i++) {
-    if (strcmp(AUTHORIZED_UIDS[i], providedUid.c_str()) == 0) {
+    if (strcmp(AUTHORIZED_UIDS[i], uid.c_str()) == 0) {
       Serial.println("Authorized");
       return true;
     }
@@ -64,4 +59,11 @@ String getUid(MFRC522 rfid) {
     ID.concat(String(rfid.uid.uidByte[i], HEX));
   }
   return ID;
+}
+
+void printAuthorizedIds() {
+  Serial.println("Authorized UIDs:");
+  for (int i = 0; i < sizeof(AUTHORIZED_UIDS) / sizeof(AUTHORIZED_UIDS[0]); i++) {
+    Serial.println(AUTHORIZED_UIDS[i]);
+  }
 }
